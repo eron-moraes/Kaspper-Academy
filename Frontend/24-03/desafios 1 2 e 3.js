5a007082e1ac80bf7cc8a5f3d6279b25cb2464fd
Eron Moraes <eron.moraes7@gmail.com>
2023-03-24 21:01:30 -0300

    desafios

---
+++ Frontend/24-03/desafios 1 2 e 3.js
@@ -0,0 +1,15 @@
+// soma de duas strings
+
+var n1 = String ('Digite um número ')
+var n2 = String ('Digite outro número')
+var soma = n1 + n2
+console.log('A soma dos valor é, '+ soma)
+a=soma;
+
+// soma de dois números
+let a = Number ('Resultado da soma anterior')
+let n3 = Number ('Digite um Número para somar')
+let soma2 = a + n3
+console.log('A soma dos números é '+soma2)
+
+// Verificando se o número é par ou impar
