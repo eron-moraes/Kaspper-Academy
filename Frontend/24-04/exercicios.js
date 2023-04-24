a63c3f7400de375d027c3e9ed82989f8b1a583bf
Eron Moraes <eron.moraes7@gmail.com>
2023-04-24 20:49:00 -0300

    24-04
    com exercicio proposto

---
+++ Frontend/24-04/exercicios.js
@@ -0,0 +1,20 @@
+//Exercício 1
+for (let i = 10; i >= 1; i--) {
+    console.log(i);
+  }
+//Exercício 2
+  for (let i = 1; i <= 10; i++) {
+    let resultado = 7 * i;
+    console.log("7 x " + i + " = " + resultado);
+  }
+
+//Exercício 3
+  const n = 6.75; // Defina aqui o valor de N
+
+  for (let i = 1; i <= n; i++) {
+    console.log(i);
+  }
+  
+
+  
+  
