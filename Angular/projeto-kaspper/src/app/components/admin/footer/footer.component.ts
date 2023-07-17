57abd855cbe4d94059fe36f5c342603890e9d0c5
Eron Moraes <eron.moraes7@gmail.com>
2023-07-17 20:54:47 -0300

    footer

--- Angular/projeto-kaspper/src/app/components/admin/footer/footer.component.ts
+++ Angular/projeto-kaspper/src/app/components/admin/footer/footer.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss'],
-  standalone: true
+  standalone: true,
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule],
 })
 export class FooterComponent {
 
